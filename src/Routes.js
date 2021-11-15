e307efe71809fe342cff026f306d83cf83158fcd
Guilherme <guilhermerg1234@gmail.com>
2021-11-15 13:57:47 -0300

    criando api

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import NotFound from './pages/NotFound';
+import SignIn from "./pages/SignIn"
 
 const Rotas = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route element={<NotFound />} />
     </Routes>
   );
 };
--- src/helpes/AuthHandler.js
+++ src/helpes/AuthHandler.js
@@ -4,3 +4,11 @@ export const isLogged = () => { //verifica se o cara ta logado ou não.
     let token = Cookies.get('token'); //pega nos cookies um carinha que se chama token.
     return (token) ? true : false; //retorna true caso token exista, se não retorna false.
 }
+
+export const doLogin = (token, rememberPassword = false) => {
+    if (rememberPassword) {
+        Cookies.set('token', token, { expires: 999 });
+    } else {
+        Cookies.set('token', token);
+    }
+}
